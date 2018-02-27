d9a2c3c48ea62d3fc8effeb8bf47d2c8e749796b
Sergey Vinogradov <grapesh@gmail.com>
2018-02-27 10:30:18 -0500

    Update post.py

--- hsofs/post.py
+++ hsofs/post.py
@@ -9,9 +9,6 @@ import os,sys
 import argparse
 import glob
 import csdlpy
-import matplotlib
-matplotlib.use('Agg',warn=False)
-import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 
@@ -36,52 +33,6 @@ def read_cmd_argv (argv):
     args = parser.parse_args()    
     print '[info]: hsofs_post.py is configured with :', args
     return args
-
-
-#==============================================================================
-def plot_maxele (maxele, tracks, advTrk, grid, coast, pp, titleStr, plotFile):
-    
-    # Default plotting limits, based on advisory track, first position
-    lonlim = advTrk['lon'][0]-3.5, advTrk['lon'][0]+3.5
-    latlim = advTrk['lat'][0]-0.5, advTrk['lat'][0]+6.5
-    clim   = 0.,4.5
-    try:
-        lonlim = float(pp['Limits']['lonmin']),float(pp['Limits']['lonmax'])
-        latlim = float(pp['Limits']['latmin']),float(pp['Limits']['latmax'])
-        clim   = float(pp['Limits']['cmin']),  float(pp['Limits']['cmax'])
-    except: #default limits, in case if not specified in ini file
-        pass
-    # Find maximal maxele value within the coord limits
-    maxmax = np.max(maxele['value'][np.where( \
-                   (lonlim[0] <= maxele['lon']) & (maxele['lon'] <= lonlim[1]) & \
-                   (latlim[0] <= maxele['lat']) & (maxele['lat'] <= latlim[1]))])
-    lonmax = maxele['lon'][np.where(maxele['value']==maxmax)]
-    latmax = maxele['lat'][np.where(maxele['value']==maxmax)]
-    print '[info]: max maxele = ',str(maxmax),'at ',str(lonmax),'x',str(latmax)
-        
-    f = csdlpy.plotter.plotMap(lonlim, latlim, fig_w=10., coast=coast)
-    csdlpy.plotter.addSurface (grid, maxele['value'],clim=clim)
-    
-    csdlpy.plotter.plotTrack(advTrk, color='k',linestyle='--',markersize=1,zorder=11)
-    if type(tracks) is list:
-        for t in tracks:
-            csdlpy.plotter.plotTrack(t,  color='r',linestyle='-',markersize=1,zorder=10)
-    else:
-        csdlpy.plotter.plotTrack(tracks, color='r',linestyle='-',markersize=1,zorder=10)
-    
-    plt.text (lonlim[0]+0.01, latlim[0]+0.01, titleStr )
-    maxStr = 'MAX VAL='+ str(np.round(maxmax,1)) + ' '
-    try:
-        maxStr = maxStr + pp['General']['units'] +', '+ pp['General']['datum']
-    except:
-        pass # in case if there is a problem with pp
-    plt.text (lonlim[0]+0.01, latlim[1]-0.1, maxStr)
-        
-    plt.plot(lonmax, latmax, 'ow',markerfacecolor='k',markersize=10)
-    plt.plot(lonmax, latmax, 'ow',markerfacecolor='r',markersize=5)
-    plt.text (lonmax,latmax, str(np.round(maxmax,1)),color='k',fontsize=10)
-    csdlpy.plotter.save(titleStr, plotFile)
-    plt.close(f) 
     
 #==============================================================================
 def run_post(argv):
